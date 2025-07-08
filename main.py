3d4138fab25e15d66558940f9b93657be1fdf799
George Puryear <gm.puryear@yahoo.com>
2025-07-08 12:02:09 -0500

    added schdeuler to reun bot every 10 seconds

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import requests 
 from bs4 import BeautifulSoup
 import json
-import wm_bot
 import target_bot
+import time
 
 def main():
-    #Target bot
-    target_bot.get_target_product_data()
+    while True:
+        #target bot
+        target_bot.get_target_product_data()
+        time.sleep(10) # Delay for 5 seconds
 
 if __name__ == "__main__":
     main()
